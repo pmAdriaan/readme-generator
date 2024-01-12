33d2cc5353e64edae0c25bb95e9a59555edacdc8
pmAdriaan <pmAdriaan@users.noreply.github.com>
2024-01-12 18:59:42 +0000

    Added questions to the questions array

--- index.js
+++ index.js
@@ -5,7 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Enter the title of your project:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter installation instructions:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information:",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your application:",
+        choices: ["MIT", "Apache-2.0", "GPL-3.0", "BSD-3-Clause", "None"],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter contribution guidelines:",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter test instructions:",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address:",
+    },
 ];
 
 // function to write README file
@@ -14,7 +59,20 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+        ])
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
 }
 
 // function call to initialize program
